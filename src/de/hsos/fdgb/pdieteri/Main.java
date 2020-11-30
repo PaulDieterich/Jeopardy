0e7bc98dcd85b0811872545cb5e36675e8495af6
pjd <pjd-popos@pop-os.localdomain>
2020-11-30 10:41:09 +0100

    outsource gamelogic

---
+++ src/de/hsos/fdgb/pdieteri/Main.java
@@ -0,0 +1,12 @@
+package de.hsos.fdgb.pdieteri;
+
+
+import de.hsos.fdgb.pdieteri.jeopardy.Spiel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Spiel Jeopardy = new Spiel();
+        Jeopardy.Start();
+    }
+}
